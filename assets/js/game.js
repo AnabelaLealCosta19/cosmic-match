ea65e6e294654544bb14ed33f2953b123b38d585
AnabelaLealCosta19 <anabela.leal.costa@gmail.com>
2020-08-15 22:35:32 +0000

    Write function to shuffle cards

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,27 @@
-
+let activeCards = document.getElementsByClassName("card");
 
 // Align card-front and card-back on top of each other
 for (let n=1; n<13; n++) {
     // Add alignment to parent div of each card
     let parent = document.getElementsByClassName(`card-${n}`)[0];
-    console.log(parent);
     parent.classList.add("card-alignment-parent");
     // Add alignment to card-front and card-back
     let childFront = document.getElementsByClassName(`card-front-${n}`)[0]; 
-    console.log(childFront);
     childFront.classList.add("card-alignment-child");
     let childBack = document.getElementsByClassName(`card-back-${n}`)[0];
-    console.log(childBack);
     childBack.classList.add("card-alignment-child");
-}
+};
+
+// Shuffle Cards
+ 
+(function shuffle (){
+    for (let n=1; n<13; n++) {
+        let randomNumber = Math.round(Math.random()*12);
+        let currentCard = document.getElementsByClassName(`card-${n}`)[0];
+        console.log(currentCard);
+        currentCard.style.order = randomNumber;
+    };
+})();
+
+
 
