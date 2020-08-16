a5c955f256412e40d7e3d94ae1b93141fb5a13db
AnabelaLealCosta19 <anabela.leal.costa@gmail.com>
2020-08-16 15:24:56 +0000

    Clean up code for matching cards

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,9 +3,9 @@
 let activeCards = Array.from(document.getElementsByClassName("card"));
 
 let hasFlippedCard = false;
+
 let firstCard;
 let secondCard;
-
 let firstCardType;
 let secondCardType;
 
@@ -33,7 +33,7 @@ activeCards.forEach(activeCards => {
 // Flip and select Cards
 
 // Flip Cards | source: 'Memory Card Game - JavaScript Tutorial - freecodecamp' (URL: https://www.youtube.com/watch?v=ZniVgo8U7ek&t=298s) 
-function selectCard () {
+function checkCard () {
     // Flip and add selected styling to card
     let selectedChildren = Array.from(this.children);
     if (selectedChildren[0].classList.contains("selected")) {
@@ -62,32 +62,34 @@ function selectCard () {
     // Check if cards match
     if (firstCardType !== undefined && secondCardType !== undefined) {
         if (firstCardType == secondCardType) {
-            console.log("We have a match!")
-            setTimeout(() => {
-                let matchedChildren = Array.from(firstCard.children).concat(Array.from(secondCard.children));
-                matchedChildren.forEach(matchedChildren => matchedChildren.classList.remove("selected"));
-                console.log(matchedChildren);
-                // Reset all first and second card variables
-                firstCard = secondCard = firstCardType = secondCardType = undefined;
-                console.log(firstCard, secondCard, firstCardType, secondCardType);
-            }, 750);
+            setTimeout(cardsMatch, 750);
         } else {
-            setTimeout(() => {
-                firstCard.classList.remove("flip");
-                secondCard.classList.remove("flip");
-                let matchedChildren = Array.from(firstCard.children).concat(Array.from(secondCard.children));
-                matchedChildren.forEach(matchedChildren => matchedChildren.classList.remove("selected"));
-                console.log(matchedChildren);
-                // Reset all first and second card variables
-                firstCard = secondCard = firstCardType = secondCardType = undefined;
-                console.log(firstCard, secondCard, firstCardType, secondCardType);
-            }, 750);
-            console.log("Try again!")
+            setTimeout(noMatch, 750);
         };
     };
     
 };
 
-activeCards.forEach(activeCards => activeCards.addEventListener('click', selectCard));
+function cardsMatch() {
+    // Remove highlighted border
+    let matchedChildren = Array.from(firstCard.children).concat(Array.from(secondCard.children));
+    matchedChildren.forEach(matchedChildren => matchedChildren.classList.remove("selected"));
+    // Reset all first and second card variables
+    firstCard = secondCard = firstCardType = secondCardType = undefined;
+};
+
+function noMatch() {
+    // Turn cards back over
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    // Remove highlighted border
+    let matchedChildren = Array.from(firstCard.children).concat(Array.from(secondCard.children));
+    matchedChildren.forEach(matchedChildren => matchedChildren.classList.remove("selected"));
+    // Reset all first and second card variables
+    firstCard = secondCard = firstCardType = secondCardType = undefined;
+};
+
+// Event listener for click on cards
+activeCards.forEach(activeCards => activeCards.addEventListener('click', checkCard));
 
 
