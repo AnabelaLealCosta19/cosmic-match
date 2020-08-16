db77c231c0a19afe78a91c0839c93da9b7437f35
AnabelaLealCosta19 <anabela.leal.costa@gmail.com>
2020-08-16 12:06:29 +0000

    Add function to determine card type

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,13 @@
+// Global Variables
+
 let activeCards = Array.from(document.getElementsByClassName("card"));
+
+let hasFlippedCard = false;
+let firstCard = undefined;
+let secondCard = undefined;
+
+let firstCardType;
+let secondCardType;
 
 
 // Align card-front and card-back on top of each other
@@ -25,6 +34,7 @@ activeCards.forEach(activeCards => {
 
 // Flip Cards | source: 'Memory Card Game - JavaScript Tutorial - freecodecamp' (URL: https://www.youtube.com/watch?v=ZniVgo8U7ek&t=298s) 
 function selectCard () {
+    // Flip and add selected styling to card
     let selectedChildren = Array.from(this.children);
     if (selectedChildren[0].classList.contains("selected")) {
         console.log("Already selected!");
@@ -33,6 +43,25 @@ function selectCard () {
         this.classList.add("flip");
         selectedChildren.forEach(selectedChildren => selectedChildren.classList.add("selected"));
     }
+    // Define if card is firstCard or secondCard
+    // Define if card is firstCard or secondCard | source: 'Memory Card Game - JavaScript Tutorial - freecodecamp' (URL: https://www.youtube.com/watch?v=ZniVgo8U7ek&t=298s) 
+    if (!hasFlippedCard){
+        // First click
+        hasFlippedCard = true;
+        firstCard = this;
+        // Remove last character from id | source: stackoverflow' (URL: https://stackoverflow.com/questions/1794822/remove-last-character-in-id-attribute) 
+        firstCardType = firstCard.id.substr(0, this.id.length -1);
+        console.log({hasFlippedCard, firstCard, firstCardType});
+    } else {
+        secondCard = this;
+        secondCardType = secondCard.id.substr(0, this.id.length -1);
+        console.log({hasFlippedCard, secondCard, secondCardType});
+    };
+
+    // Check if cards match
+    if (firstCardType !== undefined && secondCardType !== undefined) {
+        console.log("We can start checking if cards match! :)");
+    };
     
 }
 
